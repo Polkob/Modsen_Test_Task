2dc3fb416415fdfbb9ca431a2a4cbc472abeed79
polina <polyasuper2.0@gmail.com>
2024-06-19 15:02:03 +0300

    card

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,34 +2,34 @@ import react, { useState } from "react";
 import Modal from "./Modal";
 
 const Card = ({ book }) => {
-    
+
     const [show, setShow] = useState(false);
     const [bookItem, setItem] = useState();
     console.log(book);
     return (
         <>
             {
-                book.map((item)=>{
-                    
-                    let thumbnail=item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
+                book.map((item) => {
+
+                    let thumbnail = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
                     let amount = item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
-                    if(thumbnail!=undefined &&amount != undefined ){
-                    return(
-                        <>
-                        <div className="card" onClick={()=>{setShow(true);setItem(item)}}>
-                        <img src={thumbnail} alt="" />
-                            <div>
-                                <h3 className="title">{item.volumeInfo.title}</h3>
-                                <p className="amount">{item.volumeInfo.authors}</p>
-                            </div>
-                        </div>
-                        <div>
-                            <Modal show = {show} item={bookItem} onClose={()=>setShow(false)}/>
-                        </div>
-                        </>
-                    )
-                    }   
-            
+                    if (thumbnail !== undefined && amount !== undefined) {
+                        return (
+                            <>
+                                <div className="card" onClick={() => { setShow(true); setItem(item) }}>
+                                    <img src={thumbnail} alt="" />
+                                    <div>
+                                        <h3 className="title">{item.volumeInfo.title}</h3>
+                                        <p className="amount">{item.volumeInfo.authors}</p>
+                                    </div>
+                                </div>
+
+                                <Modal show={show} item={bookItem} onClose={() => setShow(false)} />
+
+                            </>
+                        )
+                    }
+
                 })
             }
         </>
--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,7 +1,7 @@
 import react from 'react';
-
-const Modal=({show, item, onClose})=>{
-    if(!show){
+const Modal=({show,item,onClose})=>{
+    if(!show)
+    {
         return null;
     }
     let thumbnail=item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
@@ -9,19 +9,18 @@ const Modal=({show, item, onClose})=>{
         <>
             <div className="overlay">
                 <div className="overlay-inner">
-                    <button className="close" onClick={onClose}>x</button>
+                    <button className="close" onClick={onClose}><i class="fas fa-times"></i></button>
                     <div className="inner-box">
-                        <img src={thumbnail} alt=""/>
+                        <img src={thumbnail} alt="" />
                         <div className="info">
                             <h1>{item.volumeInfo.title}</h1>
                             <h3>{item.volumeInfo.authors}</h3>
-                            <h4>{item.volumeInfo.publisher} <span>{item.volumeInfo.publishedDate}</span></h4><br/>
+                            <h4>{item.volumeInfo.publisher}<span>{item.volumeInfo.publishedDate}</span></h4><br/>
                             <a href={item.volumeInfo.previewLink}><button>More</button></a>
                         </div>
                     </div>
-                    <h4 className="desccription">{item.volumeInfo.description}</h4>
+                    <h4 className="description">{item.volumeInfo.description}</h4>
                 </div>
-
             </div>
         </>
     )
